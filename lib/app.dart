c93047fb716d7f504c631a4557c0f925a46a1472
Joseph Ampadu <jkampadujnr@gmail.com>
2023-01-26 15:22:51 +0000

    update folder structure (#1)

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'pages/home/home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Calculator'),
+    );
+  }
+}
