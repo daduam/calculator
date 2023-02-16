2661b275ac135c6ae7eb099122ae1759a0b6f3d1
Joseph Ampadu <jkampadujnr@gmail.com>
2023-02-16 15:19:11 +0000

    add ButtonType model (#10)

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../models/button_type.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
@@ -8,7 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: const Color(0xFF1C1C1C),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
@@ -36,20 +37,24 @@ class HomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.red)),
+                  child: Container(
+                      decoration:
+                          BoxDecoration(color: ButtonType.ac.backgroundColor)),
                 ),
                 Expanded(
                   child: Container(
-                      decoration: BoxDecoration(color: Colors.yellow)),
+                      decoration: BoxDecoration(
+                          color: ButtonType.plusminus.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.blue)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.percent.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.green)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.divide.backgroundColor)),
                 ),
               ],
             ),
@@ -60,19 +65,23 @@ class HomePage extends StatelessWidget {
               children: [
                 Expanded(
                   child: Container(
-                      decoration: BoxDecoration(color: Colors.yellow)),
+                      decoration: BoxDecoration(
+                          color: ButtonType.seven.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.blue)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.eight.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.green)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.nine.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.red)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.multiply.backgroundColor)),
                 ),
               ],
             ),
@@ -82,20 +91,24 @@ class HomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.blue)),
-                ),
-                Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.green)),
-                ),
-                Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.red)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.four.backgroundColor)),
                 ),
                 Expanded(
                   child: Container(
-                      decoration: BoxDecoration(color: Colors.yellow)),
+                      decoration: BoxDecoration(
+                          color: ButtonType.five.backgroundColor)),
+                ),
+                Expanded(
+                  child: Container(
+                      decoration:
+                          BoxDecoration(color: ButtonType.six.backgroundColor)),
+                ),
+                Expanded(
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.subtract.backgroundColor)),
                 ),
               ],
             ),
@@ -105,20 +118,24 @@ class HomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.green)),
-                ),
-                Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.red)),
+                  child: Container(
+                      decoration:
+                          BoxDecoration(color: ButtonType.one.backgroundColor)),
                 ),
                 Expanded(
                   child: Container(
-                      decoration: BoxDecoration(color: Colors.yellow)),
+                      decoration:
+                          BoxDecoration(color: ButtonType.two.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.blue)),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: ButtonType.three.backgroundColor)),
+                ),
+                Expanded(
+                  child: Container(
+                      decoration:
+                          BoxDecoration(color: ButtonType.add.backgroundColor)),
                 ),
               ],
             ),
@@ -130,15 +147,18 @@ class HomePage extends StatelessWidget {
                 Expanded(
                   flex: 2,
                   child: Container(
-                      decoration: BoxDecoration(color: Colors.yellow)),
+                      decoration: BoxDecoration(
+                          color: ButtonType.zero.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.blue)),
+                  child: Container(
+                      decoration:
+                          BoxDecoration(color: ButtonType.dot.backgroundColor)),
                 ),
                 Expanded(
-                  child:
-                      Container(decoration: BoxDecoration(color: Colors.green)),
+                  child: Container(
+                      decoration:
+                          BoxDecoration(color: ButtonType.eq.backgroundColor)),
                 ),
               ],
             ),
