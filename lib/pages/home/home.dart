406ad7f072488152c3b27355a1fed98efc3eb133
Joseph Ampadu <jkampadujnr@gmail.com>
2023-03-05 15:48:59 +0000

    responsive calculator buttons (#12)

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -11,84 +11,84 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF1C1C1C),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            flex: 2,
-            child: Container(),
-          ),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.all(8.0),
-              alignment: Alignment.bottomRight,
-              child: Text(
-                "0",
-                maxLines: 1,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 80,
-                  fontWeight: FontWeight.w300,
+      body: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(8.0),
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  "0",
+                  maxLines: 1,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 80,
+                    fontWeight: FontWeight.w300,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(child: CalculatorButton(ButtonType.ac)),
-                Expanded(child: CalculatorButton(ButtonType.plusminus)),
-                Expanded(child: CalculatorButton(ButtonType.percent)),
-                Expanded(child: CalculatorButton(ButtonType.divide)),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(child: CalculatorButton(ButtonType.seven)),
-                Expanded(child: CalculatorButton(ButtonType.eight)),
-                Expanded(child: CalculatorButton(ButtonType.nine)),
-                Expanded(child: CalculatorButton(ButtonType.multiply)),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(child: CalculatorButton(ButtonType.four)),
-                Expanded(child: CalculatorButton(ButtonType.five)),
-                Expanded(child: CalculatorButton(ButtonType.six)),
-                Expanded(child: CalculatorButton(ButtonType.subtract)),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(child: CalculatorButton(ButtonType.one)),
-                Expanded(child: CalculatorButton(ButtonType.two)),
-                Expanded(child: CalculatorButton(ButtonType.three)),
-                Expanded(child: CalculatorButton(ButtonType.add)),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(child: CalculatorButton(ButtonType.zero)),
-                Expanded(child: CalculatorButton(ButtonType.dot)),
-                Expanded(child: CalculatorButton(ButtonType.eq)),
-              ],
-            ),
-          ),
-        ],
+            ...calculatorButtons(context),
+          ],
+        ),
       ),
     );
+  }
+
+  List<Widget> calculatorButtons(BuildContext context) {
+    List<List<ButtonType>> buttonTypes = [
+      [
+        ButtonType.ac,
+        ButtonType.plusminus,
+        ButtonType.percent,
+        ButtonType.divide
+      ],
+      [
+        ButtonType.seven,
+        ButtonType.eight,
+        ButtonType.nine,
+        ButtonType.multiply
+      ],
+      [ButtonType.four, ButtonType.five, ButtonType.six, ButtonType.subtract],
+      [ButtonType.one, ButtonType.two, ButtonType.three, ButtonType.add],
+      [ButtonType.zero, ButtonType.dot, ButtonType.eq]
+    ];
+
+    var buttonWidgets = List<Widget>.empty(growable: true);
+    var size = (MediaQuery.of(context).size.width - 60.0) * 0.25;
+
+    for (final row in buttonTypes) {
+      var rowWidgets = List<Widget>.empty(growable: true);
+
+      for (final buttonType in row) {
+        var isWide = buttonType == ButtonType.zero;
+
+        rowWidgets.add(
+          SizedBox(
+            width: isWide ? (2 * size + 12.0) : size,
+            height: size,
+            child: CalculatorButton(buttonType, isWide),
+          ),
+        );
+      }
+
+      buttonWidgets.add(
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: rowWidgets,
+          ),
+        ),
+      );
+    }
+
+    return buttonWidgets;
   }
 }
